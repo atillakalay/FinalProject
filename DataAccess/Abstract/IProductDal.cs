a90ce504128d3e3395f1d97bb4cdf78c929b964c
atilla kalay <atilla.kalayy@gmail.com>
2021-02-07 01:01:04 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        List<ProductDetailDto> GetProductDetails();
+    }
+}
+
+//Code Refactoring
