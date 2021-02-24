bb5ceeaee1a8382006366093b59e6ec5106b704f
atilla kalay <atilla.kalayy@gmail.com>
2021-02-25 00:07:48 +0300

    24.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,14 +15,14 @@ namespace ConsoleUI
             ProductTest();
         }
 
-        private static void CategoryTest()
-        {
-            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
-            {
-                Console.WriteLine(category.CategoryName);
-            }
-        }
+        //private static void CategoryTest()
+        //{
+        //    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        //    foreach (var category in categoryManager)
+        //    {
+        //        Console.WriteLine(category.CategoryName);
+        //    }
+        //}
 
         private static void ProductTest()
         {
